466d25f630ae44e39d704dc313b410e83b987037
Shaun Gorllapati <shaunjgorllapati@gmail.com>
2024-08-17 17:00:29 -0400

    Uploading files

---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// import components
+import Container from "../components/Container";
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Hello world</h1>
+    </Container>
+  )
+}
